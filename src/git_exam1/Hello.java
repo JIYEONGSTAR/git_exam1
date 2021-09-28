2cffa87a8cd06b204930c10f9bee5ae7a0fb98b9
jiyeong <jiyeongstar@gmail.com>
2021-09-28 09:20:54 +0900

    201814092-4

--- src/git_exam1/Hello.java
+++ src/git_exam1/Hello.java
@@ -3,9 +3,9 @@ package git_exam1;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("IT융합자율학부");
-		System.out.println("201814092");
-		System.out.println("박지영");
+		System.out.println("소속: IT융합자율학부");
+		System.out.println("학번: 201814092");
+		System.out.println("이름: 박지영");
 	}
 
 }
